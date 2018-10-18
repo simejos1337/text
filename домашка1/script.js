0f1df45cb1fba386b1018ff03ce5dd236e3207ad
Maxim <simejos@gmail.com>
2018-10-18 15:28:30 +0300

    Це наш перший коміт

---
+++ домашка1/script.js
@@ -0,0 +1,13 @@
+var budget = +prompt("Ваш бюджет?");
+var marketName = prompt("Назва вашого магазину?");
+var mainList = {
+	budget: budget,
+	marketName: marketName,
+	shopGoods: []
+};
+
+var shopGoods = prompt("Какой товар будем продавать?");
+var shopGoods = prompt("Какой товар будем продавать?");
+var shopGoods = prompt("Какой товар будем продавать?");
+
+alert("Ваш бюджет на день = " + Math.round(budget / 30) + рублей);
